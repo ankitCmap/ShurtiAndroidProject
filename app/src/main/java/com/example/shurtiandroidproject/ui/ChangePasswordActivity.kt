c518848b8acfd12feb3dec32b96b132547db0543
A004900 <arun.bharti@in.mbdalchemie.com>
2023-04-25 18:27:25 +0530

    resourse update

--- app/src/main/java/com/example/shurtiandroidproject/ui/ChangePasswordActivity.kt
+++ app/src/main/java/com/example/shurtiandroidproject/ui/ChangePasswordActivity.kt
@@ -4,24 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.example.shurtiandroidproject.R
+import com.example.shurtiandroidproject.databinding.ActivityChangePasswordBinding
 import com.example.shurtiandroidproject.databinding.ActivityLoginSignupBinding
 import com.example.shurtiandroidproject.helper.BasePreferencesManager
 import com.example.shurtiandroidproject.roomdatabase.BaseActivity
 
-class LoginSignupActivity :BaseActivity() {
+class ChangePasswordActivity :BaseActivity() {
 
-    lateinit var binding:ActivityLoginSignupBinding
+    lateinit var binding:ActivityChangePasswordBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=DataBindingUtil.setContentView(this, R.layout.activity_login_signup)
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_change_password)
         initView()
     }
 
     private fun initView() {
-        binding.tvskip.setOnClickListener {
+        binding.btnUpdatePassword.setOnClickListener {
 //            BasePreferencesManager.putBoolean(BasePreferencesManager.IS_SKIP,true)
-            val intent = Intent(this@LoginSignupActivity, ChangePasswordActivity::class.java)
+            val intent = Intent(this@ChangePasswordActivity, MainActivity::class.java)
             startActivity(intent)
             finish()
 
